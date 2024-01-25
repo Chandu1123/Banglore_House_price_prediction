c80b08cb0b1ab7935b0a31a1204add54d9e6fc61
Chandu1123 <chandusarithala555@gmail.com>
2024-01-25 20:00:17 +0530

    code updated

--- BANGL_HOUSE_PRICE/SERVER/server.py
+++ BANGL_HOUSE_PRICE/SERVER/server.py
@@ -1,9 +1,9 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 import util
 
 app = Flask(__name__)
 
-@app.route('/get_location_names')
+@app.route('/get_location_names', methods=['GET'])
 def get_location_names():
     response = jsonify({
         'locations': util.get_location_names()
@@ -12,6 +12,7 @@ def get_location_names():
 
     return response
 
+@app.route('/predict_home_price', methods=['GET', 'POST'])
 def predict_home_price():
     total_sqft = float(request.form['total_sqft'])
     location = request.form['location']
@@ -25,7 +26,7 @@ def predict_home_price():
 
     return response
 
-
-if __name__=='__main__':
-    print('starting flask server for predictions...')
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    util.load_saved_artifacts()
     app.run()
